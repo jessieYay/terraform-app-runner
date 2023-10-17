ac168d0d346ab288b3f014f26e0d2fae8b23e038
EC2 Default User <ec2-user@ip-172-31-39-253.eu-west-1.compute.internal>
2023-10-17 09:34:08 +0000

    added provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "glenn.richard.bech/apprunner-a-new-state.state"
+    key    = "jessie_s_unik/apprunner-a-new-state.state"
     region = "eu-north-1"
   }
 }
